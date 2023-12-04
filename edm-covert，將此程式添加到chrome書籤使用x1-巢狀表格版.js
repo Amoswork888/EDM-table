557e3f90fd945f522eea5f62b5e057d5050ce967
Amos <amos.work888@gmail.com>
2023-12-05 01:11:40 +0800

    增加功能：轉換完表格後，自動複製到剪貼簿

--- edm-covert，將此程式添加到chrome書籤使用x1-巢狀表格版.js
+++ edm-covert，將此程式添加到chrome書籤使用x1-巢狀表格版.js
@@ -64,5 +64,30 @@ javascript:(function() {
         subTable.style.maxWidth = imgMaxWidth + 'px';
         subTable.style.textAlign = 'center';
     });
+    
 
+    /*找到table元素*/
+    let tableToCopy = document.querySelector('table');
+
+    /* 確認是否找到table元素 */
+    if (tableToCopy) {
+        /* 複製table及其子代的原始碼 */
+        let tableHtml = tableToCopy.outerHTML;
+        /* 建立一個textarea元素，並將複製的HTML放入其中 */
+        let textarea = document.createElement('textarea');
+        textarea.value = tableHtml;
+        /* 將textarea元素添加到body中 */
+        document.body.appendChild(textarea);
+        /* 選中textarea中的內容 */
+        textarea.select();
+        /* 執行複製到剪貼簿的命令 */
+        document.execCommand('copy');
+        /* 移除textarea元素 */
+        document.body.removeChild(textarea);
+
+        alert('已複製table元素到剪貼簿');
+    } else {
+        alert('未找到table元素');
+    }
+    
 })();
