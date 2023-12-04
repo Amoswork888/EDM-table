977e35878987c1b32a06d2c00b623dfd30447463
Amos <amos.work888@gmail.com>
2023-12-05 00:36:46 +0800

    功能變更：依據圖片自動設定表格寬度
    表格不再寫死寬度，全部採用圖片為依據，1倍時抓取最寬的圖片作為表格width，2倍時將最大圖片寬度除2後寫入表格width

--- edm-covert，將此程式添加到chrome書籤使用x1-巢狀表格版.js
+++ edm-covert，將此程式添加到chrome書籤使用x1-巢狀表格版.js
@@ -1,37 +1,24 @@
 javascript:(function() {
 
+    let bodyElement = document.querySelector('body');
     let tableElement = document.querySelector('table');
     let subTableElement = document.querySelectorAll('table table');
     let imgElements = document.querySelectorAll('img');
+    let imgMaxWidth = 0; /*建立變數存放最大的圖片尺寸，以利設定為 table 寬度*/
 
-    tableElement.setAttribute('width', '600');
-    tableElement.setAttribute('align', 'center');
-    tableElement.setAttribute('cellpadding', '0');
-    tableElement.setAttribute('cellspacing', '0');
-    tableElement.setAttribute('border', '0');
-    tableElement.style.backgroundColor = '#ffffff';
-    tableElement.style.width = '100%';
-    tableElement.style.maxWidth = '600px';
-    tableElement.style.margin = 'auto';
-
-    subTableElement.forEach(function(subTable) {
-        subTable.setAttribute('align', 'center');
-        subTable.setAttribute('cellpadding', '0');
-        subTable.setAttribute('cellspacing', '0');
-        subTable.setAttribute('border', '0');
-        subTable.setAttribute('border', '0');
-        subTable.style.backgroundColor = '#ffffff';
-        subTable.style.width = '100%';
-        subTable.style.maxWidth = '600px';
-    });
-
+    /*遍歷所有圖片，抓取尺寸並設定到 img 及 td*/
     imgElements.forEach(function(imgElement) {
         let tdParent = imgElement.closest('td');
         let imgWidth = imgElement.width;
         let imgHeight = imgElement.height;
+        /*比較所有圖片尺寸，抓出最寬的圖片尺寸給表格使用*/
+        if (imgWidth > imgMaxWidth) {
+            imgMaxWidth = imgWidth;
+        }
 
         imgElement.setAttribute('width', imgWidth);
         imgElement.setAttribute('height', imgHeight);
+        imgElement.setAttribute('align', 'center');
         imgElement.style.verticalAlign = 'top';
         imgElement.style.display = 'block';
         imgElement.style.margin = '0';
@@ -39,13 +26,43 @@ javascript:(function() {
         imgElement.style.border = 'none';
 
         if (tdParent) {
-
             tdParent.setAttribute('width', imgWidth);
             tdParent.setAttribute('height', imgHeight);
             tdParent.setAttribute('border', '0');
+            tdParent.setAttribute('align', 'center');
             tdParent.style.width = imgWidth + 'px';
             tdParent.style.height = imgHeight + 'px';
             tdParent.style.padding = '0';
         }
     });
+
+    /*設定 body*/
+    bodyElement.setAttribute('leftmargin', '0');
+    bodyElement.setAttribute('topmargin', '0');
+    bodyElement.setAttribute('marginwidth', '0');
+    bodyElement.setAttribute('marginheight', '0');
+
+    /*設定主 table*/
+    tableElement.setAttribute('width', imgMaxWidth);
+    tableElement.setAttribute('align', 'center');
+    tableElement.setAttribute('cellpadding', '0');
+    tableElement.setAttribute('cellspacing', '0');
+    tableElement.setAttribute('border', '0');
+    tableElement.style.backgroundColor = '#ffffff';
+    tableElement.style.width = '100%';
+    tableElement.style.maxWidth = imgMaxWidth + 'px';
+    tableElement.style.margin = '20px auto';
+    tableElement.style.textAlign = 'center';
+    /*設定巢狀 table */
+    subTableElement.forEach(function(subTable) {
+        subTable.setAttribute('align', 'center');
+        subTable.setAttribute('cellpadding', '0');
+        subTable.setAttribute('cellspacing', '0');
+        subTable.setAttribute('border', '0');
+        subTable.style.backgroundColor = '#ffffff';
+        subTable.style.width = '100%';
+        subTable.style.maxWidth = imgMaxWidth + 'px';
+        subTable.style.textAlign = 'center';
+    });
+
 })();
